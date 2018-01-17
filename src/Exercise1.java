911beeda8a4362d339168a4681002bee7d3fa7a2
Jon Fincher <jfincher42@gmail.com>
2018-01-16 21:54:58 -0800

    Initial check-in

---
+++ src/Exercise1.java
@@ -0,0 +1,11 @@
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		System.out.println("x is " + x);
+		x = 10;
+		System.out.println("x is now " + x);
+	}
+
+}
---
+++ src/Exercise2.java
@@ -0,0 +1,11 @@
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = str1;
+		str1 = "Goodbye";
+		System.out.println(str2);
+	}
+
+}
